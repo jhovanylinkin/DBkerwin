3a9bdf2d095f731c93a45ff0fed0763182541b65
Jhovany <jhovanymb.info@gmail.com>
2016-11-27 08:46:02 -0600

    cambios agregando la clase de ConexionSQL

--- MiTiendita/MiTiendita/Agregar.cs
+++ MiTiendita/MiTiendita/Agregar.cs
@@ -16,8 +16,78 @@ namespace MiTiendita
         {
             InitializeComponent();
         }
+        //public Productos productoActual { get; set; }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Guardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(IDAgregar.Text) || string.IsNullOrWhiteSpace(NombreAgregar.Text) ||
+                            string.IsNullOrWhiteSpace(UnidadAgregar.Text)||string.IsNullOrWhiteSpace(PrecioAgregar.Text)|| string.IsNullOrWhiteSpace(CantidadCantidad.Text))
+
+                MessageBox.Show("Hay Uno o mas Campos Vacios!", "Campos Vacios!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            else
+            {
+
+                Productos pProductos = new Productos();
+                int integrarID,integrarPieza;//conversion de string  int
+                double integrarPrecio;
+                integrarID = Convert.ToInt32(IDAgregar.Text);
+                integrarID = int.Parse(IDAgregar.Text);
+                integrarPieza = Convert.ToInt32(CantidadCantidad.Text);
+                integrarPieza = int.Parse(CantidadCantidad.Text);
+                integrarPrecio = Convert.ToDouble(PrecioAgregar.Text);
+                integrarPrecio = double.Parse(PrecioAgregar.Text); 
+
+                pProductos.idProducto = integrarID;
+                pProductos.nombre = NombreAgregar.Text.Trim();
+                pProductos.unidanes = UnidadAgregar.Text.Trim();
+                pProductos.precio = integrarPrecio;
+               // pProductos.Fecha_Nacimiento = dateTimePicker1.Value.Year + "/" + dateTimePicker1.Value.Month + "/" + dateTimePicker1.Value.Day;
+                pProductos.pieza = integrarPieza;
+             
+
+                int resultado = ProductosIME.AgregarProductos(pProductos);
+                if (resultado > 0)
+                {
+                    MessageBox.Show("Cliente Guardado Con Exito!!", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //Limpiar();
+                    //Deshabilitar();
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo guardar el cliente", "Fallo!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+
+            }
+        }
+
+        private void IDAgregar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NombreAgregar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UnidadAgregar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrecioAgregar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CantidadCantidad_TextChanged(object sender, EventArgs e)
         {
 
         }
--- MiTiendita/MiTiendita/Principal.cs
+++ MiTiendita/MiTiendita/Principal.cs
@@ -7,19 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace MiTiendita
 {
     public partial class Principal : Form
     {
+        
+        MySqlConnection conexion = new MySqlConnection();
+
         public Principal()
         {
+
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-        
+            Agregar i = new Agregar();
+            i.Show();
+        }
+
+        private void Buscar_Click(object sender, EventArgs e)
+        {
+            Buscar i = new Buscar();
+            i.Show();
+        }
+        public void abrircconeccion()
+        {
+
+            string cadenaconeccion = "Server=localhost;Database=mydb; user id=root; password=fundamentos";
+            conexion.ConnectionString = cadenaconeccion;
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conectado");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("no conectado" + ex);
+            }
+        }
+
+        public void cerrarconeccion()
+        {
+            try
+            {
+                conexion.Close();
+                MessageBox.Show("desconectado");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("no desconectado" + ex);
+            }
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            abrircconeccion();
+        }
+
+        private void Desconectar_Click(object sender, EventArgs e)
+        {
+            cerrarconeccion();
         }
     }
 }
