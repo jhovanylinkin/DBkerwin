ed0b14d242203a9a407da7d48b00b5cf66a0758a
MauricioSak <Halo_mauricio@hotmail.com>
2016-11-28 22:33:35 -0600

    AHO ESTAN TODAS LAS INTERFACES CHECALO

---
+++ MiTiendita/MiTiendita/Cliente.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MiTiendita
+{
+    partial class Cliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AgregarCliente = new System.Windows.Forms.Button();
+            this.BuscarCliente = new System.Windows.Forms.Button();
+            this.BorrarCliente = new System.Windows.Forms.Button();
+            this.NombreLb = new System.Windows.Forms.Label();
+            this.TelefonoLb = new System.Windows.Forms.Label();
+            this.DireccionLb = new System.Windows.Forms.Label();
+            this.NombreCliente = new System.Windows.Forms.TextBox();
+            this.TelefonoCliente = new System.Windows.Forms.TextBox();
+            this.DireccionCliente = new System.Windows.Forms.TextBox();
+            this.Inicio = new System.Windows.Forms.Button();
+            this.ListaCliente = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ListaCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AgregarCliente
+            // 
+            this.AgregarCliente.Location = new System.Drawing.Point(86, 263);
+            this.AgregarCliente.Name = "AgregarCliente";
+            this.AgregarCliente.Size = new System.Drawing.Size(75, 23);
+            this.AgregarCliente.TabIndex = 0;
+            this.AgregarCliente.Text = "Agregar";
+            this.AgregarCliente.UseVisualStyleBackColor = true;
+            // 
+            // BuscarCliente
+            // 
+            this.BuscarCliente.Location = new System.Drawing.Point(190, 263);
+            this.BuscarCliente.Name = "BuscarCliente";
+            this.BuscarCliente.Size = new System.Drawing.Size(75, 23);
+            this.BuscarCliente.TabIndex = 1;
+            this.BuscarCliente.Text = "Buscar";
+            this.BuscarCliente.UseVisualStyleBackColor = true;
+            // 
+            // BorrarCliente
+            // 
+            this.BorrarCliente.Location = new System.Drawing.Point(304, 263);
+            this.BorrarCliente.Name = "BorrarCliente";
+            this.BorrarCliente.Size = new System.Drawing.Size(75, 23);
+            this.BorrarCliente.TabIndex = 2;
+            this.BorrarCliente.Text = "Borrar";
+            this.BorrarCliente.UseVisualStyleBackColor = true;
+            // 
+            // NombreLb
+            // 
+            this.NombreLb.AutoSize = true;
+            this.NombreLb.Location = new System.Drawing.Point(36, 52);
+            this.NombreLb.Name = "NombreLb";
+            this.NombreLb.Size = new System.Drawing.Size(44, 13);
+            this.NombreLb.TabIndex = 3;
+            this.NombreLb.Text = "Nombre";
+            // 
+            // TelefonoLb
+            // 
+            this.TelefonoLb.AutoSize = true;
+            this.TelefonoLb.Location = new System.Drawing.Point(36, 106);
+            this.TelefonoLb.Name = "TelefonoLb";
+            this.TelefonoLb.Size = new System.Drawing.Size(49, 13);
+            this.TelefonoLb.TabIndex = 4;
+            this.TelefonoLb.Text = "Telefono";
+            // 
+            // DireccionLb
+            // 
+            this.DireccionLb.AutoSize = true;
+            this.DireccionLb.Location = new System.Drawing.Point(36, 162);
+            this.DireccionLb.Name = "DireccionLb";
+            this.DireccionLb.Size = new System.Drawing.Size(52, 13);
+            this.DireccionLb.TabIndex = 5;
+            this.DireccionLb.Text = "Direccion";
+            // 
+            // NombreCliente
+            // 
+            this.NombreCliente.Location = new System.Drawing.Point(94, 49);
+            this.NombreCliente.Name = "NombreCliente";
+            this.NombreCliente.Size = new System.Drawing.Size(100, 20);
+            this.NombreCliente.TabIndex = 6;
+            // 
+            // TelefonoCliente
+            // 
+            this.TelefonoCliente.Location = new System.Drawing.Point(94, 99);
+            this.TelefonoCliente.Name = "TelefonoCliente";
+            this.TelefonoCliente.Size = new System.Drawing.Size(100, 20);
+            this.TelefonoCliente.TabIndex = 7;
+            // 
+            // DireccionCliente
+            // 
+            this.DireccionCliente.Location = new System.Drawing.Point(94, 155);
+            this.DireccionCliente.Name = "DireccionCliente";
+            this.DireccionCliente.Size = new System.Drawing.Size(100, 20);
+            this.DireccionCliente.TabIndex = 8;
+            // 
+            // Inicio
+            // 
+            this.Inicio.Location = new System.Drawing.Point(526, 300);
+            this.Inicio.Name = "Inicio";
+            this.Inicio.Size = new System.Drawing.Size(75, 23);
+            this.Inicio.TabIndex = 9;
+            this.Inicio.Text = "Inicio";
+            this.Inicio.UseVisualStyleBackColor = true;
+            // 
+            // ListaCliente
+            // 
+            this.ListaCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListaCliente.Location = new System.Drawing.Point(263, 12);
+            this.ListaCliente.Name = "ListaCliente";
+            this.ListaCliente.Size = new System.Drawing.Size(356, 245);
+            this.ListaCliente.TabIndex = 10;
+            // 
+            // Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 335);
+            this.Controls.Add(this.ListaCliente);
+            this.Controls.Add(this.Inicio);
+            this.Controls.Add(this.DireccionCliente);
+            this.Controls.Add(this.TelefonoCliente);
+            this.Controls.Add(this.NombreCliente);
+            this.Controls.Add(this.DireccionLb);
+            this.Controls.Add(this.TelefonoLb);
+            this.Controls.Add(this.NombreLb);
+            this.Controls.Add(this.BorrarCliente);
+            this.Controls.Add(this.BuscarCliente);
+            this.Controls.Add(this.AgregarCliente);
+            this.Name = "Cliente";
+            this.Text = "Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.ListaCliente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AgregarCliente;
+        private System.Windows.Forms.Button BuscarCliente;
+        private System.Windows.Forms.Button BorrarCliente;
+        private System.Windows.Forms.Label NombreLb;
+        private System.Windows.Forms.Label TelefonoLb;
+        private System.Windows.Forms.Label DireccionLb;
+        private System.Windows.Forms.TextBox NombreCliente;
+        private System.Windows.Forms.TextBox TelefonoCliente;
+        private System.Windows.Forms.TextBox DireccionCliente;
+        private System.Windows.Forms.Button Inicio;
+        private System.Windows.Forms.DataGridView ListaCliente;
+    }
+}
---
+++ MiTiendita/MiTiendita/Proveedores.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MiTiendita
+{
+    partial class Proveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AgregarProveedor = new System.Windows.Forms.Button();
+            this.BuscarProveedor = new System.Windows.Forms.Button();
+            this.BorrarProveedor = new System.Windows.Forms.Button();
+            this.Inicio = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NombreProveedor = new System.Windows.Forms.TextBox();
+            this.RFCProveedor = new System.Windows.Forms.TextBox();
+            this.DireccionProveedor = new System.Windows.Forms.TextBox();
+            this.TelefonoProveedor = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AgregarProveedor
+            // 
+            this.AgregarProveedor.Location = new System.Drawing.Point(45, 329);
+            this.AgregarProveedor.Name = "AgregarProveedor";
+            this.AgregarProveedor.Size = new System.Drawing.Size(75, 23);
+            this.AgregarProveedor.TabIndex = 0;
+            this.AgregarProveedor.Text = "Agregar";
+            this.AgregarProveedor.UseVisualStyleBackColor = true;
+            // 
+            // BuscarProveedor
+            // 
+            this.BuscarProveedor.Location = new System.Drawing.Point(138, 329);
+            this.BuscarProveedor.Name = "BuscarProveedor";
+            this.BuscarProveedor.Size = new System.Drawing.Size(75, 23);
+            this.BuscarProveedor.TabIndex = 1;
+            this.BuscarProveedor.Text = "Buscar";
+            this.BuscarProveedor.UseVisualStyleBackColor = true;
+            // 
+            // BorrarProveedor
+            // 
+            this.BorrarProveedor.Location = new System.Drawing.Point(228, 329);
+            this.BorrarProveedor.Name = "BorrarProveedor";
+            this.BorrarProveedor.Size = new System.Drawing.Size(67, 23);
+            this.BorrarProveedor.TabIndex = 2;
+            this.BorrarProveedor.Text = "Borrar";
+            this.BorrarProveedor.UseVisualStyleBackColor = true;
+            // 
+            // Inicio
+            // 
+            this.Inicio.Location = new System.Drawing.Point(613, 333);
+            this.Inicio.Name = "Inicio";
+            this.Inicio.Size = new System.Drawing.Size(75, 23);
+            this.Inicio.TabIndex = 3;
+            this.Inicio.Text = "Inicio";
+            this.Inicio.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RFC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Direccion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefono";
+            // 
+            // NombreProveedor
+            // 
+            this.NombreProveedor.Location = new System.Drawing.Point(76, 34);
+            this.NombreProveedor.Name = "NombreProveedor";
+            this.NombreProveedor.Size = new System.Drawing.Size(263, 20);
+            this.NombreProveedor.TabIndex = 8;
+            // 
+            // RFCProveedor
+            // 
+            this.RFCProveedor.Location = new System.Drawing.Point(228, 61);
+            this.RFCProveedor.Name = "RFCProveedor";
+            this.RFCProveedor.Size = new System.Drawing.Size(100, 20);
+            this.RFCProveedor.TabIndex = 9;
+            // 
+            // DireccionProveedor
+            // 
+            this.DireccionProveedor.Location = new System.Drawing.Point(407, 34);
+            this.DireccionProveedor.Name = "DireccionProveedor";
+            this.DireccionProveedor.Size = new System.Drawing.Size(281, 20);
+            this.DireccionProveedor.TabIndex = 10;
+            // 
+            // TelefonoProveedor
+            // 
+            this.TelefonoProveedor.Location = new System.Drawing.Point(76, 61);
+            this.TelefonoProveedor.Name = "TelefonoProveedor";
+            this.TelefonoProveedor.Size = new System.Drawing.Size(100, 20);
+            this.TelefonoProveedor.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(618, 239);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Proveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 364);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TelefonoProveedor);
+            this.Controls.Add(this.DireccionProveedor);
+            this.Controls.Add(this.RFCProveedor);
+            this.Controls.Add(this.NombreProveedor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Inicio);
+            this.Controls.Add(this.BorrarProveedor);
+            this.Controls.Add(this.BuscarProveedor);
+            this.Controls.Add(this.AgregarProveedor);
+            this.Name = "Proveedores";
+            this.Text = "Proveedores";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AgregarProveedor;
+        private System.Windows.Forms.Button BuscarProveedor;
+        private System.Windows.Forms.Button BorrarProveedor;
+        private System.Windows.Forms.Button Inicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NombreProveedor;
+        private System.Windows.Forms.TextBox RFCProveedor;
+        private System.Windows.Forms.TextBox DireccionProveedor;
+        private System.Windows.Forms.TextBox TelefonoProveedor;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
